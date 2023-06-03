ea06a8883b3a5cbff234299981fa17660c966a18
juan <rmz7619a@gmail.com>
2023-06-02 19:06:04 -0600

    Correcion funcion encriptador y desencriptador 4

--- encriptar.js
+++ encriptar.js
@@ -4,6 +4,7 @@ var text2="";
 
 var respuesta=true;
 var acceder=false;
+var encriptar1=false;
 
 function limpiar(){
 
@@ -50,6 +51,7 @@ function encriptar2(){
     if(text1.length > 0 ){
 
         var contador=0;
+        var contador3=0;
 
         for(var x = 0; x < text1.length; x++) {
             var caracter=text1.charAt(x);
@@ -64,21 +66,49 @@ function encriptar2(){
                 contador=0;
                 break;
             }
+
+            if(caracter =="a" || caracter =="e" || caracter =="i" || caracter =="o" ||caracter =="u"){
+
+                contador3=contador3 + 1;
+            }
         }
 
         console.log(contador);
 
         if(contador>0){
-            var color= document.getElementById("nota");
-            color.style.color="black";
-    
-            respuesta=false;
-            visible(respuesta);
 
-            buscar(text1);
+            var color= document.getElementById("nota");
+
+            if(contador3>0){
+      
+                color.style.color="black";
+        
+                respuesta=false;
+                visible(respuesta);
     
-            original=text1;
-            acceder=true;
+                buscar(text1);      //ECRIPTA
+        
+                original=text1;
+                acceder=true;
+                encriptar1=true;
+            }
+            else{
+                alert("¡¡ESTA FRACE NO CONTIENE: 'a', 'e', 'i', 'o', 'u', POR LO TANTO EL TEXTO NO SE ENCRIPRARA!!");
+
+                color.style.color="black";
+        
+                respuesta=false;
+                visible(respuesta);
+    
+                document.getElementById("area2").innerHTML="NULL: EL TEXTO NO SE PUEDE ENCRIPTAR.";     //NO ENCRIPTA
+           
+
+                respuesta=false;
+                original=text1;
+                acceder=true;
+                encriptar1=true;
+            }
+
         }
         else{
 
@@ -87,7 +117,7 @@ function encriptar2(){
             var advertencia= document.getElementById("nota");
             advertencia.style.color="red";
 
-
+            alert("ADVERTENIA: LEE LAS LETRAS ROJAS")
 
             visible(respuesta);
         }
@@ -115,26 +145,57 @@ function desencriptar2(){
         else{
 
             if(validar1>0 && validar2>0){
-                if(text2 != text1){
 
-                    document.getElementById("area2").innerHTML=original;
-                    respuesta=false;
-                    visible(respuesta);   /*  RESUELTO */
+                var contador2=0;
+
+                for(var x = 0; x < text2.length; x++) {
+                    var caracter2=text1.charAt(x);
+                    
+                    
+
+                    if(caracter2 =="a" || caracter2 =="e" || caracter2 =="i" || caracter2 =="o" ||caracter2 =="u"){
+
+                        contador2=contador2 + 1;
+                    }
+
+       
+                }
+
+                if(contador2>0){
+        
+                    if(text2 != text1){
+
+                        document.getElementById("area2").innerHTML=original;
+                        respuesta=false;
+                        visible(respuesta);   /*  RESUELTO */
+                    }
+                    else{
+                        alert("EL TEXTO YA ESTA DESENCRIPTADO.")
+                    }
                 }
                 else{
-                    alert("EL texto ya esta desencriptado 2.")
+                    alert("¡Esta opcion no esta disponible!")
                 }
             }
 
             if(validar1==0 && validar2>0){
-                if(text2==original){
-                    alert("EL TEXTO YA ESTA des-ENCRIPTADO 1");
+
+                if(encriptar1==true){
+                    if(text2==original){
+                        alert("EL TEXTO YA ESTA DES-ENCRIPTADO");
+                    }
+                    else{
+                        document.getElementById("area2").innerHTML=original;
+                        respuesta=false;
+                        visible(respuesta);   /*  RESUELTO */
+
+                        encriptar1=false;
+                    }
                 }
                 else{
-                    document.getElementById("area2").innerHTML=original;
-                    respuesta=false;
-                    visible(respuesta);   /*  RESUELTO */
+                    alert("PRIMERO DEBES ENCRIPTAR.");
                 }
+
             }
 
             if(validar2==0 && validar1>0){
@@ -145,11 +206,11 @@ function desencriptar2(){
         }
     }
     else{
-        alert("DEBES ENCRIPTAR PRIMERO 1.");
+        alert("DEBES ENCRIPTAR PRIMERO.");
     }
 }
 
-  //-------------------------------------------------------------->
+//-------------------------------------------------------------->
 
 function copiar2(){
     var texto_copiado = document.getElementById('area2').innerHTML;         //ALAMACENA LA INFO//
